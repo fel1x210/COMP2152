03260a1cd268fdb6e7b534b6f816e4dbb723f851
Nguyen Minh Triet Luu <luunguyenminhtriet021025@gmail.com>
2025-01-13 14:45:06 -0500

    update lab01.py coding questions

---
+++ week01/lab01.py
@@ -0,0 +1,21 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Nguyen Minh Triet
+# Last Name: Luu
+
+# Question 1:
+array_variable = [1, 4, 7, 9]
+
+# Question 2:
+a = 1
+b = 2
+c = 3
+d = 4
+e = (a - ((b ** c) // d)) + (a % c)
+
+# Question 3:
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Question 4:
+userAge = int(input("Enter your age: "))
+print ("Now showing the shop items filtered by age : ", userAge)
